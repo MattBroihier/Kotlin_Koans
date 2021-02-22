b516166c9277c0d03eca3fce21f0839717a31168
MattBroihier <mattbroihier@gmail.com>
2021-02-21 20:37:00 -0600

    Trying some code in a worksheet

--- TryingThingsOut.ws.kts
+++ TryingThingsOut.ws.kts
@@ -12,3 +12,14 @@ println (FirstFloat + SecondFloat == ThirdFloat)
 println (FirstFloat + SecondFloat - ThirdFloat)
 
 println (FirstFloat)
+
+var b: String? = "abc" // can be set null
+b = null // ok
+print(b)
+
+val b: String? = "Kotlin"
+if (b != null && b.length > 0) {
+    print("String of length ${b.length}")
+} else {
+    print("Empty string")
+}
